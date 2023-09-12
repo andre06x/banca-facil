d932f2b145a4554bbc7b8666832dd2903eed5aba
Luccas <luccas.marqu3s@gmail.com>
2023-09-11 21:39:49 -0300

    Update estabelecimentoController

--- server/src/controllers/EstabelecimentoController.js
+++ server/src/controllers/EstabelecimentoController.js
@@ -2,27 +2,36 @@ import EstabelecimentoService from "../services/EstabelecimentoService.js";
 
 class EstabelecimentoController {
   async criarEstabelecimento(req, res) {
-    const Estabelecimento = await EstabelecimentoService.criarEstabelecimento(req);
+    const Estabelecimento = await EstabelecimentoService.criarEstabelecimento(
+      req
+    );
     return res.status(Estabelecimento.status).json(Estabelecimento);
   }
 
   async buscarEstabelecimento(req, res) {
-    const Estabelecimento = await EstabelecimentoService.buscarEstabelecimento(req);
+    const Estabelecimento = await EstabelecimentoService.buscarEstabelecimento(
+      req
+    );
     return res.status(Estabelecimento.status).json(Estabelecimento);
   }
 
   async buscarTodosEstabelecimento(req, res) {
-    const Estabelecimento = await EstabelecimentoService.buscarTodosEstabelecimentos(req);
+    const Estabelecimento =
+      await EstabelecimentoService.buscarTodosEstabelecimentos(req);
     return res.status(Estabelecimento.status).json(Estabelecimento);
   }
 
   async editarEstabelecimento(req, res) {
-    const Estabelecimento = await EstabelecimentoService.editarEstabelecimento(req);
+    const Estabelecimento = await EstabelecimentoService.editarEstabelecimento(
+      req
+    );
     return res.status(Estabelecimento.status).json(Estabelecimento);
   }
 
   async excluirEstabelecimento(req, res) {
-    const Estabelecimento = await EstabelecimentoService.excluirEstabelecimento(req);
+    const Estabelecimento = await EstabelecimentoService.excluirEstabelecimento(
+      req
+    );
     return res.status(Estabelecimento.status).json(Estabelecimento);
   }
 }
