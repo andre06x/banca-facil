42e87f307ab699a4b0e9dfef6aa9be55cd1046ad
André Silva <andre06x@gmail.com>
2023-09-16 23:28:57 -0300

    adicionando modelo produtos e sync

--- server/src/config/db/initialData.js
+++ server/src/config/db/initialData.js
@@ -4,6 +4,7 @@ import Estabelecimento from "../../models/Estabelecimento.js";
 import FuncionariosEstabelecimento from "../../models/FuncionariosEstabelecimento.js";
 import Categorias from "../../models/Categorias.js";
 import TiposPagamento from "../../models/TiposPagamento.js";
+import Produtos from "../../models/Produtos.js";
 
 const senha = await bcrypt.hash("123456", 10);
 
@@ -25,6 +26,18 @@ const tipos = [
   { tipo: "Pix" },
 ];
 
+const produtos = [
+  {
+    nome: "iphone",
+    foto: "d",
+    valor: 30.9,
+    quantidade_disponivel: 1,
+    estabelecimento_id: "c1e6a4af-9ba9-4fa9-abed-f075f4a6c3aa",
+    categoria_id: "6e01541a-abf5-4f1c-a6ca-9dad61e8ae36",
+    taxas_id: "6e01541a-abf5-4f1c-a6ca-9dad61e8ae37",
+  },
+];
+
 export async function createInitialData() {
   try {
     await Usuario.sync({ alter: true });
@@ -32,8 +45,10 @@ export async function createInitialData() {
     await FuncionariosEstabelecimento.sync({ alter: true });
     await Categorias.sync({ alter: true });
     await TiposPagamento.sync({ alter: true });
+    await Produtos.sync({ alter: true });
     await Usuario.bulkCreate(usuarios, { ignoreDuplicates: true });
     await TiposPagamento.bulkCreate(tipos, { ignoreDuplicates: true });
+    await Produtos.bulkCreate(produtos, { ignoreDuplicates: true });
   } catch (err) {
     console.log("Err" + err.message);
   }
