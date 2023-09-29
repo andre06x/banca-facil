ebf9422cadc3a49b780fdbd875c78c754524218e
André Silva <andre06x@gmail.com>
2023-09-29 10:36:15 -0300

    alterando ordem rotas

--- server/routes.js
+++ server/routes.js
@@ -8,6 +8,8 @@ import { RotaCategoria } from "./src/routes/RotaCategoria.js";
 import { RotaProduto } from "./src/routes/RotaProduto.js";
 import { RotaTipoPagamento } from "./src/routes/RotaTipoPagamento.js";
 import { RotaTaxa } from "./src/routes/RotaTaxa.js";
+import { RotaProdutoTaxa } from "./src/routes/RotaProdutoTaxa.js";
+import { RotaVendaStatus } from "./src/routes/RotaVendaStatus.js";
 
 const router = new Router();
 
@@ -15,13 +17,15 @@ router.post("/api/usuario", UsuarioController.criarUsuario);
 router.get("/api/usuario/autenticar", UsuarioController.autenticar);
 
 router.use(RotaUsuario);
-router.use(RotaEstabelecimento);
-router.use(RotaFuncionarioEstabelecimento);
 router.use(RotaCategoria);
 router.use(RotaProduto);
 router.use(RotaTipoPagamento);
 router.use(RotaTaxa);
+router.use(RotaProdutoTaxa);
+router.use(RotaVendaStatus);
+router.use(RotaFuncionarioEstabelecimento);
 router.use(CheckToken);
+router.use(RotaEstabelecimento);
 
 //produtos
 
