3e09913dad5996a4e7842b2aff308d06557dffdb
André Silva <andre06x@gmail.com>
2023-11-11 13:55:16 -0300

    comentando rota usuario

--- server/src/routes/RotaUsuario.js
+++ server/src/routes/RotaUsuario.js
@@ -3,7 +3,7 @@ import UsuarioController from "../controllers/UsuarioController.js";
 
 const RotaUsuario = new Router();
 
-RotaUsuario.get("/api/usuario", UsuarioController.buscarTodosUsuarios);
+//RotaUsuario.get("/api/usuario", UsuarioController.buscarTodosUsuarios);
 RotaUsuario.get("/api/usuario/:id", UsuarioController.buscarUsuario);
 RotaUsuario.put("/api/usuario/:id", UsuarioController.editarUsuario);
 RotaUsuario.delete("/api/usuario/:id", UsuarioController.excluirUsuario);
