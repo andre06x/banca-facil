0c71b660eb7dfe1b239da63034a31c0db1a69420
André Silva <andre06x@gmail.com>
2023-11-11 09:37:00 -0300

    alterando title

--- front/pages/_document.js
+++ front/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head></Head>
+      <Head>
+        <title>BancaFácil</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
