fc595bc62bd27837249e9ca19754bbbaf538aec0
André Silva <andre06x@gmail.com>
2023-11-11 09:34:03 -0300

    adicionando produto admin

--- front/pages/estabelecimento/[...slug].js
+++ front/pages/estabelecimento/[...slug].js
@@ -7,12 +7,13 @@ import Venda from "@/components/Venda";
 import ModalCriarProduto from "@/components/ModalCriarProduto";
 import { getApiClient } from "../api/axios";
 import { formatarMoeda } from "@/utils/formatPrice";
+import { verify } from "jsonwebtoken";
 
 export async function getServerSideProps(ctx) {
   const { slug } = ctx.query;
   const estabelecimento_id = slug[0];
-  console.log(estabelecimento_id);
   const { "nextauth.token": token, "nextauth.usuario": usuario } = parseCookies(ctx);
+
   if (!token) {
     return {
       redirect: {
@@ -22,6 +23,9 @@ export async function getServerSideProps(ctx) {
       props: {},
     };
   }
+  const decode = await verify(token, "YXV0aC1hcGktc2VjcmV0LWNvbnRhaW5lci0xMjM0NTY=");
+  const admin = decode?.auth.admin;
+
   var estabelecimento = null;
 
   const apiWeb = getApiClient(ctx, token);
@@ -33,9 +37,14 @@ export async function getServerSideProps(ctx) {
 
   const responseProdutos = await apiWeb.get(`/todos-produtos/${estabelecimento_id}`);
   const produtos = responseProdutos.data.content;
-  return { props: { produtos, usuario, estabelecimento } };
+  return { props: { produtos, usuario, estabelecimento, admin } };
 }
-export default function Produtos({ estabelecimento, produtos: produtosServer, usuario }) {
+export default function Produtos({
+  estabelecimento,
+  produtos: produtosServer,
+  usuario,
+  admin,
+}) {
   const [produtos, setProdutos] = useState(produtosServer);
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [modalTaxaProduto, setModalTaxaProduto] = useState(false);
@@ -62,155 +71,160 @@ export default function Produtos({ estabelecimento, produtos: produtosServer, us
   if (estabelecimento === null) return "debug";
   return (
     <div class="w-full relative overflow-x-auto shadow-md sm:rounded-lg px-1">
-      <div class="pb-4 pt-2 bg-white dark:bg-gray-900">
-        <div className="flex justify-between mx-3">
-          <span className="text-3xl text-gray-500 ">
-            Lista de produtos {estabelecimento.nome_estabelecimento}
-          </span>
-          <button
-            onClick={() => setIsOpenModal(true)}
-            className="bg-white w-10 h-10 rounded flex items-center justify-center shadow-xl"
-          >
-            <span className="text-lg font-bold">+</span>
-          </button>
-        </div>
-      </div>
-
-      <div className="block md:hidden">
-        <div class="w-full">
-          {produtos.map((produto, index) => (
-            <div
-              key={index}
-              class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 p-4 mb-4 flex flex-row justify-between items-center"
-            >
-              <div class="flex-1 mb-2 sm:mb-0">
-                <h3 class="text-lg font-semibold">{produto?.nome}</h3>
-                <p>Preço: {formatarMoeda(Number(produto.valor))}</p>
-                <p>Disponível: {produto.quantidade_disponivel}</p>
-                <p>Categoria: {produto?.categorium?.categoria}</p>
-              </div>
-
-              <div class="flex items-center">
+      <>
+        {admin ? (
+          <>
+            <div class="pb-4 pt-2 bg-white dark:bg-gray-900">
+              <div className="flex justify-between mx-3">
+                <span className="text-3xl text-gray-500 ">
+                  Lista de produtos {estabelecimento.nome_estabelecimento}
+                </span>
                 <button
-                  type="button"
-                  onClick={() => excluirProduto(produto.id)}
-                  class="ml-4"
+                  onClick={() => setIsOpenModal(true)}
+                  className="bg-white w-10 h-10 rounded flex items-center justify-center shadow-xl"
                 >
-                  <Trash2 color="red" size={20} />
-                </button>
-
-                <button
-                  type="button"
-                  class="ml-4"
-                  onClick={() => {
-                    setEditarProduto(produto);
-                    setIsOpenModal(true);
-                  }}
-                >
-                  EDITAR
+                  <span className="text-lg font-bold">+</span>
                 </button>
               </div>
             </div>
-          ))}
-        </div>
-      </div>
-
-      <div className="block overflow-x-auto">
-        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" class="px-6 py-3"></th>
-
-              <th scope="col" class="px-6 py-3">
-                Nome
-              </th>
-              <th scope="col" class="px-6 py-3">
-                Valor
-              </th>
-              <th scope="col" class="px-6 py-3">
-                Disponivel
-              </th>
-              <th scope="col" class="px-6 py-3">
-                Categoria
-              </th>
-              <th scope="col" class="px-6 py-3"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {produtos.map((produto, index) => (
-              <tr
-                key={index}
-                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
-              >
-                <th scope="col" class="px-6 py-3"></th>
-
-                <th
-                  scope="row"
-                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  {produto.nome}
-                </th>
-
-                <th
-                  scope="row"
-                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  {formatarMoeda(Number(produto.valor))}
-                </th>
-
-                <th
-                  scope="row"
-                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  {produto.quantidade_disponivel}
-                </th>
-
-                <th
-                  scope="row"
-                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  {produto?.categorium?.categoria}
-                </th>
-
-                <td class="px-6 py-4">
-                  <button
-                    type="button"
-                    onClick={() => excluirProduto(produto.id)}
-                    className="ml-4"
+            <div className="block md:hidden">
+              <div class="w-full">
+                {produtos.map((produto, index) => (
+                  <div
+                    key={index}
+                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 p-4 mb-4 flex flex-row justify-between items-center"
                   >
-                    <Trash2 color="red" size={20} />
-                  </button>
-                </td>
+                    <div class="flex-1 mb-2 sm:mb-0">
+                      <h3 class="text-lg font-semibold">{produto?.nome}</h3>
+                      <p>Preço: {formatarMoeda(Number(produto.valor))}</p>
+                      <p>Disponível: {produto.quantidade_disponivel}</p>
+                      <p>Categoria: {produto?.categorium?.categoria}</p>
+                    </div>
 
-                <td class="px-6 py-4">
-                  <button
-                    type="button"
-                    className="ml-4"
-                    onClick={() => {
-                      setEditarProduto(produto);
-                      setIsOpenModal(true);
-                    }}
-                  >
-                    EDITAR
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+                    <div class="flex items-center">
+                      <button
+                        type="button"
+                        onClick={() => excluirProduto(produto.id)}
+                        class="ml-4"
+                      >
+                        <Trash2 color="red" size={20} />
+                      </button>
 
-      <ModalCriarProduto
-        estabelecimento={estabelecimento.id}
-        setIsOpenModal={setIsOpenModal}
-        isOpenModal={isOpenModal}
-        produtos={produtos}
-        setProdutos={setProdutos}
-        usuario={usuario}
-        editarProduto={editarProduto}
-        setEditarProduto={setEditarProduto}
-        buscarTodosProdutos={buscarTodosProdutos}
-      />
+                      <button
+                        type="button"
+                        class="ml-4"
+                        onClick={() => {
+                          setEditarProduto(produto);
+                          setIsOpenModal(true);
+                        }}
+                      >
+                        EDITAR
+                      </button>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="block overflow-x-auto hidden md:block">
+              <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                    <th scope="col" class="px-6 py-3"></th>
+
+                    <th scope="col" class="px-6 py-3">
+                      Nome
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                      Valor
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                      Disponivel
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                      Categoria
+                    </th>
+                    <th scope="col" class="px-6 py-3"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {produtos.map((produto, index) => (
+                    <tr
+                      key={index}
+                      class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                    >
+                      <th scope="col" class="px-6 py-3"></th>
+
+                      <th
+                        scope="row"
+                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                      >
+                        {produto.nome}
+                      </th>
+
+                      <th
+                        scope="row"
+                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                      >
+                        {formatarMoeda(Number(produto.valor))}
+                      </th>
+
+                      <th
+                        scope="row"
+                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                      >
+                        {produto.quantidade_disponivel}
+                      </th>
+
+                      <th
+                        scope="row"
+                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                      >
+                        {produto?.categorium?.categoria}
+                      </th>
+
+                      <td class="px-6 py-4">
+                        <button
+                          type="button"
+                          onClick={() => excluirProduto(produto.id)}
+                          className="ml-4"
+                        >
+                          <Trash2 color="red" size={20} />
+                        </button>
+                      </td>
+
+                      <td class="px-6 py-4">
+                        <button
+                          type="button"
+                          className="ml-4"
+                          onClick={() => {
+                            setEditarProduto(produto);
+                            setIsOpenModal(true);
+                          }}
+                        >
+                          EDITAR
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            <ModalCriarProduto
+              estabelecimento={estabelecimento.id}
+              setIsOpenModal={setIsOpenModal}
+              isOpenModal={isOpenModal}
+              produtos={produtos}
+              setProdutos={setProdutos}
+              usuario={usuario}
+              editarProduto={editarProduto}
+              setEditarProduto={setEditarProduto}
+              buscarTodosProdutos={buscarTodosProdutos}
+            />
+          </>
+        ) : (
+          ""
+        )}
+      </>
 
       {estabelecimento ? (
         <Venda estabelecimento={estabelecimento} produtos={produtos} />
