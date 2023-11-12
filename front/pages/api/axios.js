ca81bade772d8affe64991c8516635ac8a68d896
André Silva <andre06x@gmail.com>
2023-11-12 10:14:26 -0300

    adicionando nova valizadacao token

--- front/pages/api/axios.js
+++ front/pages/api/axios.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 import { parseCookies } from "nookies";
+import Cookies from "js-cookie";
 
 //interceptar refetch token
 export function getApiClient(ctx, tokenClient) {
-  const { "nextauth.token": token } = parseCookies(ctx);
+  let { "nextauth.token": token } = parseCookies(ctx ? ctx : null);
+
   const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
   });
 
-  console.log(api);
   api.interceptors.request.use((config) => {
     console.log(config);
     return config;
@@ -19,6 +20,13 @@ export function getApiClient(ctx, tokenClient) {
   }
   if (tokenClient) {
     api.defaults.headers["Authorization"] = ` Bearer ${tokenClient}`;
+  }
+
+  if (!token && !tokenClient) {
+    console.log("entrou");
+    const tokenDoCookie = Cookies.get("nextauth.token");
+
+    api.defaults.headers["Authorization"] = ` Bearer ${tokenDoCookie}`;
   }
   return api;
 }
